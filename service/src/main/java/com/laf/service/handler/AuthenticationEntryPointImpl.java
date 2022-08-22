69393169a20362a51ee0a15ae23ce4cbf9c87a1b
kamisora <1210281722@qq.com>
2022-08-22 22:58:43 +0800

    优化代码

--- service/src/main/java/com/laf/service/handler/AuthenticationEntryPointImpl.java
+++ service/src/main/java/com/laf/service/handler/AuthenticationEntryPointImpl.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        ResponseResult result = new ResponseResult(HttpStatus.UNAUTHORIZED.value(), "认证失败请重新登录");
+        ResponseResult result = new ResponseResult<>(HttpStatus.UNAUTHORIZED.value(), "认证失败，无法访问服务器资源，请重试！");
         String json = JSON.toJSONString(result);
         WebUtils.renderString(response, json);
     }
